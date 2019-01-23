16ed275212aae07cea0833efebb1e2793fca6980
rachit147 <46746957+rachit147@users.noreply.github.com>
2019-01-23 16:05:23 +0530

    Update Transportation on vacation.js

--- Chapter_1_DataType/Transportation on vacation.js
+++ Chapter_1_DataType/Transportation on vacation.js
@@ -3,7 +3,6 @@ function rentalCarCost(totalDays) {
     if (totalDays < 3) {
         finalAmount = totalDays * 40;
     } else if (totalDays >= 3 && totalDays < 7) {
-
         finalAmount = totalDays * 40 - 20;
     } else {
         finalAmount = totalDays * 40 - 50;
