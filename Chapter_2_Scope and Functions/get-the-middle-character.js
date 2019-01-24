6e8f45bc1f9749d1ef93fb3fdd974d99ca9a8088
rachit147 <46746957+rachit147@users.noreply.github.com>
2019-01-24 17:59:10 +0530

    Update get-the-middle-character.js

--- Chapter_2_Scope and Functions/get-the-middle-character.js
+++ Chapter_2_Scope and Functions/get-the-middle-character.js
@@ -1,12 +1,12 @@
-unction getMiddle(s) {
- var lengthOfString,middleIndex,middleCharacters;
+function getMiddle(s) {
+    var lengthOfString, middleIndex, middleCharacters;
     lengthOfString = s.length;
     middleIndex = Math.round(lengthOfString / 2);
     middleCharacters = [];
-    if (lengthOfString % 2 === 0) {
-        middleCharacters = s.charAt(middleIndex - 1) + s.charAt(middleIndex);
-    } else {
+    if (lengthOfString % 2) {
         middleCharacters = s.charAt(middleIndex - 1);
+    } else {
+        middleCharacters = s.charAt(middleIndex - 1) + s.charAt(middleIndex);
     }
     return middleCharacters;
 }
