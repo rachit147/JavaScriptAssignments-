151f663e2a63f8b386bb9e9894c9a764afc721e7
rachit147 <46746957+rachit147@users.noreply.github.com>
2019-01-24 18:03:12 +0530

    Update Task1.js

--- chapter_3/Task1.js
+++ chapter_3/Task1.js
@@ -1,11 +1,12 @@
 function prefill(n, v) {
-     if (n === Infinity || n === -Infinity || n % 1 !== 0 || n < 0 || n === true || n === false || isNaN(n)) {
+    var result;
+    if (n === Infinity || n === -Infinity || n % 1 !== 0 || n < 0 || n === true || n === false || isNaN(n)) {
         throw new TypeError(n + ' is invalid');
     } else if (n == 0) {
-        return [];
+        result = [];
     } else {
-        var arr = new Array(n);
-        arr.fill(v);
-        return arr;
+        var prefillArray = new Array(n);
+        result = prefillArray.fill(v);
     }
+    return result;
 }
