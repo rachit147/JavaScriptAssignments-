481065de12d58cd06e233ea1156defd8cce75020
rachit147 <46746957+rachit147@users.noreply.github.com>
2019-01-24 17:49:13 +0530

    Update partition-on.js

--- Chapter_2_Scope and Functions/partition-on.js
+++ Chapter_2_Scope and Functions/partition-on.js
@@ -1,7 +1,6 @@
 function partitionOn(pred, items) {
-    var arrayLength = items.length;
-    var length = 0,
-        temporary;
+    var arrayLength, temporary, length = 0;
+    arrayLength = items.length;
     for (var index = 0; index < arrayLength; index++) {
         if (!pred(items[index])) {
             temporary = items.splice(index, 1);
