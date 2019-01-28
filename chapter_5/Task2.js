68f5e50f07c8882c8b33eb2f707011582c51e99e
rachit147 <46746957+rachit147@users.noreply.github.com>
2019-01-28 11:22:20 +0530

    Update Task2.js

--- chapter_5/Task2.js
+++ chapter_5/Task2.js
@@ -1,8 +1,14 @@
 Object.prototype.hash = function(string) {
     var str = string.split('.'),
         result = this;
-    while (str.length != 0 && result !== undefined) {
-        result = result[str.shift()];
+    for (var i = 0; i < str.length; i++) {
+        if (result) {
+            console.log(result);
+            result = result[str[i]];
+            console.log(result);
+        } else {
+            break;
+        }
     }
     return result;
 };
