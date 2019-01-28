39063f9b24d50befec66e721bf1f1a0d45d0eb15
rachit147 <46746957+rachit147@users.noreply.github.com>
2019-01-28 11:12:43 +0530

    Update Task4.js

--- chapter_5/Task4.js
+++ chapter_5/Task4.js
@@ -1,5 +1,5 @@
 function isSantaClausable(obj) {
-    let status;false;
+    let status=false;
     if (typeof obj.distributeGifts === 'function' && typeof obj.goDownTheChimney === 'function' && typeof obj.sayHoHoHo === 'function') {
         status = true;
     }
